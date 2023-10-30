426e86005442154e8d8a2a0beb4a10069a965d14
Julien <larroquejl@gmail.com>
2023-10-30 14:17:57 -0400

    questions re-done and packages required

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please enter a description of your project',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide installation instructions for your project',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage instructions for your project',
+    },
+    {
+      type: 'select',
+      name: 'license',
+      message: 'Select a license',
+      choices: [
+        {
+          name: 'MIT',
+          value: 'MIT',
+        }
+      ]
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please provide contribution guidelines',
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Please enter your github username',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data,null,'\t'), (err) => err ? console.log(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    prompt(questions)
+    .then((answers) => writeToFile('README.md',answers))
+}
 
 // Function call to initialize app
 init();
