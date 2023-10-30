7cad73a345dbf7fc3d6158d63921cccd08e37dbe
Julien <larroquejl@gmail.com>
2023-10-30 14:25:14 -0400

    update to generateMarkdown and index.js

--- index.js
+++ index.js
@@ -1,62 +1,127 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+// Requre FS to be able to write files
+const fs = require("fs/promises");
+// Requre Inquirer to be able to ask questions to the user
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
+//Array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is your project title?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please enter a description of your project',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Please provide installation instructions for your project',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Please provide usage instructions for your project',
-    },
-    {
-      type: 'select',
-      name: 'license',
-      message: 'Select a license',
-      choices: [
-        {
-          name: 'MIT',
-          value: 'MIT',
-        }
-      ]
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Please provide contribution guidelines',
-    },
-    {
-      type: 'input',
-      name: 'username',
-      message: 'Please enter your github username',
-    },
+  {
+    type: "input",
+    message: "Project title: ",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Description of the project: ",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Installation instructions: ",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Usage instructions: ",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Select a license: ",
+    choices: [
+      {
+        name: "MIT License",
+        value: {
+          title: "MIT License",
+          badge:
+            "![Static Badge](https://img.shields.io/badge/license-MIT_License-green)",
+          url: "https://opensource.org/license/mit/",
+          text: "This program is covered under the MIT License.",
+        },
+      },
+      {
+        name: "Apache License 2.0",
+        value: {
+          title: "Apache License 2.0",
+          badge:
+            "![Static Badge](https://img.shields.io/badge/license-Apache_License_2.0-green)",
+          url: "https://www.apache.org/licenses/LICENSE-2.0",
+          text: "This program is covered under the Apache License 2.0.",
+        },
+      },
+      {
+        name: "GNU General Public License v3.0",
+        value: {
+          title: "GNU General Public License v3.0",
+          badge:
+            "![Static Badge](https://img.shields.io/badge/license-GNU_General_Public_License_v3.0-green)",
+          url: "https://www.gnu.org/licenses/gpl-3.0.en.html#license-text",
+          text: "This program is covered under the GNU General Public License v3.0.",
+        },
+      },
+      {
+        name: "Mozilla Public License 2.0",
+        value: {
+          title: "Mozilla Public License 2.0",
+          badge:
+            "![Static Badge](https://img.shields.io/badge/license-Mozilla_Public_License_2.0-green)",
+          url: "https://www.mozilla.org/en-US/MPL/2.0/",
+          text: "This program is covered under the Mozilla Public License 2.0.",
+        },
+      },
+      {
+        name: "Eclipse Public License 2.0",
+        value: {
+          title: "Eclipse Public License 2.0",
+          badge:
+            "![Static Badge](https://img.shields.io/badge/license-Eclipse_Public_License_2.0-green)",
+          url: "https://www.eclipse.org/legal/epl-2.0/",
+          text: "This program is covered under the Eclipse Public License 2.0.",
+        },
+      },
+    ],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "How can the user contribute to the project?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Test information: ",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "GitHub username: ",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Email address: ",
+    name: "email",
+  },
 ];
 
-// TODO: Create a function to write README file
+// Pass user responses to the generateMarkdown function, save as a new file.
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, JSON.stringify(data,null,'\t'), (err) => err ? console.log(err) : console.log('Success!')
-    );
+  fs.writeFile(fileName, generateMarkdown.generateMarkdown(data))
+    .then(() => console.log("README.md Created!"))
+    .catch((err) => console.error(err));
 }
 
-// TODO: Create a function to initialize app
-const init = () => {
-    prompt(questions)
-    .then((answers) => writeToFile('README.md',answers))
+// Initialize the app with inquirer
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      writeToFile("README.md", response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,88 +1,43 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  const licenseType = license
-
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
-const generateMarkdown = ({title, description,installation, usage, license, contributing, username, email, tests, badge}) 
-=> {
-  `
-  ${badge}
-
-  
-  # ${title}
-
+function generateMarkdown({title, description, installation, usage, license, contribution, test, github, email}) {
+  return `# ${title}
+  [${license.badge}](${license.url})
   ## Description
-  
   ${description}
-  
-  ## Table of Contents (Optional)
-  
-  If your README is long, add a table of contents to make it easy for users to find what they need.
-  
+
+  ## Table of Contents
+
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
-  - [Contributing](#contributing)
+  - [How to Contribute](#how-to-contribute)
+  - [Tests](#tests)
   - [Questions](#questions)
   
-  ## Installation
   
+  ## Installation
   ${installation}
   
   ## Usage
-  
   ${usage}
   
-  To add a screenshot, create an 'assets/images' folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-  
-      ```md
-      ![alt text](assets/images/screenshot.png)
-      ```
-  
   ## License
-
-  ${license}
+  ${license.text}  
+  Full text of this license can be found here: <${license.url}>
   
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-  
-  ---
-  
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  
-  ## Badges
-  
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-  
-  ## Questions
-
-  For questions, please feel free to contact 
-  
-  ${username}
-  ${email}
-  
-  ## Contributing
-  
-  ${contributing}
+  ## How to Contribute
+  ${contribution}
   
   ## Tests
+  ${test}
   
-  ${tests}
-
+  ## Questions
+  For additional questions about this project, please see my GitHub page: <https://www.github.com/${github}>  
+  or email me at: <${email}>
+  
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports ={
+  generateMarkdown
+};
